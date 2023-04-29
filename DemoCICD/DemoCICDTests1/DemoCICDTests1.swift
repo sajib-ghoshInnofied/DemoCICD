45a781a74b204ab1254369c4690f480e7ad3c8d1
iOS991 <ghoshsajib5s@gmail.com>
2023-04-29 23:48:56 +0530

    demo test added

---
+++ DemoCICD/DemoCICDTests1/DemoCICDTests1.swift
@@ -0,0 +1,26 @@
+//
+//  DemoCICDTests1.swift
+//  DemoCICDTests1
+//
+//  Created by Sajib Ghosh on 29/04/23.
+//
+
+import XCTest
+@testable import DemoCICD
+class DemoCICDTests1: XCTestCase {
+
+    //var stringToTest = String()
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func demoTest(){
+        
+        XCTAssertEqual("test", "test1")
+    }
+
+}
---
+++ DemoCICD/DemoCICDTests1/DemoTest.swift
@@ -0,0 +1,20 @@
+//
+//  DemoTest.swift
+//  DemoCICDTests1
+//
+//  Created by Sajib Ghosh on 29/04/23.
+//
+
+import XCTest
+
+class DemoTest: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    func demoTest(){
+        XCTAssertEqual("test", "test")
+    }
+
+}
